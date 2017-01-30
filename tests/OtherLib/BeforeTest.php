acf5a8c00b9d5fb1051d11006cde66d363802367
holoflins <hulot.morgan@neuf.fr>
2017-01-30 21:28:06 +0100

    add other lib

---
+++ tests/OtherLib/BeforeTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace tests\Lib;
+
+use PHPUnit\Framework\TestCase;
+
+class BeforeTest extends TestCase
+{
+    public function testSecond()
+    {
+    }
+
+    public function testBefore()
+    {
+    }
+}
