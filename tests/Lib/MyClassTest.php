e1e24a2c85c8117effa2cb7025b2eec14255003b
holoflins <hulot.morgan@neuf.fr>
2017-02-06 11:30:01 +0100

    correction tp

--- tests/Lib/MyClassTest.php
+++ tests/Lib/MyClassTest.php
@@ -2,11 +2,49 @@
 
 namespace tests\Lib;
 
+use Lib\MyClass;
+use Lib\MyFakeClass;
 use PHPUnit\Framework\TestCase;
 
 class MyClassTest extends TestCase
 {
-    public function testMyMethod()
+    /** @var  MyFakeClass */
+    private $myFakeClass;
+
+    /** @var  array */
+    private $config;
+
+    public function setUp()
     {
+        $this->config = ['link' => 'value'];
+        $this->myFakeClass = new MyFakeClass();
+    }
+
+    public function testMyMethodWithGoodKey()
+    {
+        $key = 'link';
+        $expected = 'value';
+
+        // on lance l'action
+        // on vérifie le résultat
+    }
+
+    public function testMyMethodWithFalseKey()
+    {
+        $key = 'false';
+        $expected = null;
+
+        // on lance l'action
+        // on vérifie le résultat
+    }
+
+    public function testMyMehodWithMorganKey()
+    {
+        $this->config['morgan'] = 'ajoutez ici un GENTIL commentaire';
+        $key = 'morgan';
+        $expected = 'gros mégalo';
+
+        // on lance l'action
+        // on vérifie le résultat
     }
 }
