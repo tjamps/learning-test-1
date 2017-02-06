69de135b55c9aa4f247ca61334c6c23f31c3cd2f
holoflins <hulot.morgan@neuf.fr>
2017-02-06 11:29:12 +0100

    prepare td

--- src/Lib/MyClass.php
+++ src/Lib/MyClass.php
@@ -4,7 +4,37 @@ namespace Lib;
 
 class MyClass
 {
-    public function myMethod()
+    /** @var  MyFakeClass */
+    private $myFakeClass;
+
+    /** @var  array */
+    private $config;
+
+    /**
+     * MyClass constructor.
+     * @param MyFakeClass $myFakeClass
+     * @param array $config
+     */
+    public function __construct(MyFakeClass $myFakeClass, array $config)
     {
+        $this->myFakeClass = $myFakeClass;
+        $this->config = $config;
+    }
+
+    /**
+     * @param $value
+     * @return mixed|null
+     */
+    public function myMethod($key)
+    {
+        if (!array_key_exists($key, $this->config)) {
+            return null;
+        }
+
+        if (array_key_exists('morgan', $this->config)) {
+            return 'gros mégalo';
+        }
+
+        return $this->config[$key];
     }
 }
